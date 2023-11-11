b7e1d1556c1e024a4ab3e42fe2750f82be020266
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-11-11 13:30:02 -0600

    Refactor Hero component styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,15 +3,15 @@ import Finder from './Finder'
 /* eslint-disable @next/next/no-img-element */
 export default function Hero() {
   return (
-    <section className='text-gray-600 body-font'>
-      <div
+    <section className='relative'>
+      {/* <div
         aria-hidden='true'
         className='absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-30'
       >
         <div className='blur-[106px] h-56 bg-gradient-to-br from-blue-700 to-purple-400 dark:from-blue-700' />
         <div className='blur-[106px] h-32 bg-gradient-to-r from-cyan-600 to-purple-700 dark:to-indigo-600' />
-      </div>
-      <div className='container mx-auto flex px-5 pt-10 md:flex-row flex-col items-center  md:px-16'>
+      </div> */}
+      <div className='relative container mx-auto flex px-5 pt-10 md:flex-row flex-col items-center md:px-16'>
         <div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center z-20'>
           <div className='order-first flex items-center justify-center gap-4 text-[0.8125rem] leading-6 text-slate-500 lg:justify-start pb-4 pl-1 font-[GilroyRegular]'>
             <p>Working in the icons</p>
@@ -24,7 +24,7 @@ export default function Hero() {
             </svg>
             <p>MIT license</p>
           </div>
-          <h1 className='title-font sm:text-5xl text-3xl mb-4 text-gray-900 principal-title font-[GilroyBold]'>
+          <h1 className='sm:text-5xl text-3xl mb-4 text-gray-900 font-[GilroyBold]'>
             Beautiful SVG icons meticulously created by the talented designers
             behind windUI.
           </h1>
@@ -52,7 +52,7 @@ export default function Hero() {
             >
               <img
                 src='/svg/Figma-logo.svg'
-                alt='GitHub Logo'
+                alt='Figma Logo'
                 className='w-6 h-6 mr-2'
               />
               Figma File
@@ -61,7 +61,7 @@ export default function Hero() {
         </div>
         <div className='lg:max-w-lg lg:w-full md:w-1/2 w-5/6 hidden md:block'>
           <img
-            className='object-cover object-center rounded'
+            className='object-cover object-center rounded select-none'
             alt='hero'
             src='/svg/crafted.svg'
           />
